5ea08565a529d313d146c460b6fd313fe49f06ba
tron <sashinchetty8@gmail.com>
2024-06-09 21:14:08 +0200

    Added aphrodite-8b.sh

--- aphrodite-start.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-#https://github.com/PygmalionAI/aphrodite-engine/wiki/3.-Engine-Options#server-options
-
-# Run the aphrodite API server with the specified model
-subprocess.run(["python", "-m", "aphrodite.endpoints.openai.api_server", "--launch-kobold-api"
-                "--model", "TheBloke/Mistral-7B-v0.1-GPTQ"])
