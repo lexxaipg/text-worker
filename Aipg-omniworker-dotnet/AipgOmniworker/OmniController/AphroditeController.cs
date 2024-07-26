3fd176e16fb778e4b77121eb337e8049ffbfd4d2
mateusz.krzaczek <mateusz.krzaczek@gmail.com>
2024-07-26 18:58:34 +0200

    Containers

--- Aipg-omniworker-dotnet/AipgOmniworker/OmniController/AphroditeController.cs
+++ Aipg-omniworker-dotnet/AipgOmniworker/OmniController/AphroditeController.cs
@@ -1,6 +1,135 @@
-﻿namespace AipgOmniworker.OmniController;
+﻿using System.Diagnostics;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace AipgOmniworker.OmniController;
 
 public class AphroditeController
 {
+    public List<string> AphroditeOutput { get; private set; } = new();
+
+    public event EventHandler<string> OnAphroditeOutputChangedEvent;
+
+    public string WorkingDirectory => "/worker";
     
+    private readonly TextWorkerConfigManager _textWorkerConfigManager;
+    private Process? _aphroditeProcess;
+
+    public AphroditeController(TextWorkerConfigManager textWorkerConfigManager)
+    {
+        _textWorkerConfigManager = textWorkerConfigManager;
+    }
+
+    public async Task StarAphrodite()
+    {
+        try
+        {
+            await StartAphroditeInternal();
+        }
+        catch (Exception e)
+        {
+            PrintGridTextWorkerOutput($"Failed to start Aphrodite");
+            PrintGridTextWorkerOutput(e.ToString());
+        }
+    }
+    
+    private async Task StartAphroditeInternal()
+    {
+        PrintGridTextWorkerOutput("Starting Aphrodite...");
+
+        var textWorkerConfig = await _textWorkerConfigManager.LoadConfig();
+        string ModelName = textWorkerConfig.model_name;
+        string HuggingFaceToken = textWorkerConfig.hugging_face_token;
+        
+        Process? process = Process.Start(new ProcessStartInfo
+        {
+            FileName = "/usr/bin/python3",
+            Arguments = $"-m aphrodite.endpoints.openai.api_server",
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            //UseShellExecute = false,
+            WorkingDirectory = WorkingDirectory,
+            Environment =
+            {
+                {"DISABLE_TERMINAL_UI", "true" },
+                {"MODEL_NAME", ModelName },
+                {"HF_TOKEN", HuggingFaceToken },
+                {"KOBOLD_API", "true"},
+                {"GPU_MEMORY_UTILIZATION", textWorkerConfig.gpu_utilization.ToString(CultureInfo.InvariantCulture)
+                    .Replace(",", ".")}
+            }
+        });
+        
+        if (process == null)
+        {
+            PrintGridTextWorkerOutput("Failed to start Aphrodite");
+            return;
+        }
+
+        _aphroditeProcess = process;
+        
+        process.Exited += (sender, args) =>
+        {
+            PrintGridTextWorkerOutput($"Aphrodite exited! Exit code: {process.ExitCode}");
+        };
+        
+        process.OutputDataReceived += (sender, args) =>
+        {
+            if (args.Data != null)
+            {
+                PrintGridTextWorkerOutput(args.Data);
+            }
+        };
+
+        process.ErrorDataReceived += (sender, args) =>
+        {
+            if (args.Data != null)
+            {
+                PrintGridTextWorkerOutput(args.Data);
+            }
+        };
+        
+        process.BeginOutputReadLine();
+        process.BeginErrorReadLine();
+    }
+    
+    private void PrintGridTextWorkerOutput(string output)
+    {
+        output =  new Regex(@"\x1B\[[^@-~]*[@-~]").Replace(output, "");
+        AphroditeOutput.Add(output);
+
+        try
+        {
+            OnAphroditeOutputChangedEvent?.Invoke(this, output);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+
+    public async Task KillWorkers()
+    {
+        if(_aphroditeProcess != null && !_aphroditeProcess.HasExited)
+        {
+            _aphroditeProcess.Kill();
+        }
+
+        await WaitForExit().WaitAsync(TimeSpan.FromSeconds(10));
+    }
+
+    private async Task WaitForExit()
+    {
+        while(_aphroditeProcess != null && !_aphroditeProcess.HasExited)
+        {
+            await Task.Delay(100);
+        }
+
+        _aphroditeProcess = null;
+    }
+
+    public void ClearOutput()
+    {
+        AphroditeOutput.Clear();
+    }
 }
--- Aipg-omniworker-dotnet/AipgOmniworker/OmniController/OmniControllerMain.cs
+++ Aipg-omniworker-dotnet/AipgOmniworker/OmniController/OmniControllerMain.cs
@@ -13,13 +13,15 @@ public class OmniControllerMain
     private readonly GridWorkerController _gridWorkerController;
     private readonly BridgeConfigManager _bridgeConfigManager;
     private readonly TextWorkerConfigManager _textWorkerConfigManager;
+    private readonly AphroditeController _aphroditeController;
 
     public OmniControllerMain(GridWorkerController gridWorkerController, BridgeConfigManager bridgeConfigManager,
-        TextWorkerConfigManager textWorkerConfigManager)
+        TextWorkerConfigManager textWorkerConfigManager, AphroditeController aphroditeController)
     {
         _gridWorkerController = gridWorkerController;
         _bridgeConfigManager = bridgeConfigManager;
         _textWorkerConfigManager = textWorkerConfigManager;
+        _aphroditeController = aphroditeController;
         _gridWorkerController = gridWorkerController;
 
         _gridWorkerController.OnGridTextWorkerOutputChangedEvent += OnGridTextWorkerOutputChanged;
@@ -52,101 +54,25 @@ public class OmniControllerMain
         _startCancellation?.Cancel();
 
         await _gridWorkerController.KillWorkers();
+        await _aphroditeController.KillWorkers();
 
         _gridWorkerController.ClearOutput();
+        _aphroditeController.ClearOutput();
         Output.Clear();
 
         AddOutput("Starting worker...");
 
         _startCancellation = new CancellationTokenSource();
 
-        await StartAphrodite();
-        await WaitForAphroditeStart();
+        AddOutput("Starting Aphrodite...");
+        await _aphroditeController.StarAphrodite();
+        AddOutput("Aphrodite started!");
 
         AddOutput("Starting Grid Text Worker...");
         await _gridWorkerController.StartGridTextWorker();
         AddOutput("Grid Text Worker started!");
     }
-
-    private async Task WaitForAphroditeStart()
-    {
-        try
-        {
-            AddOutput("Waiting for Aphrodite to start...");
-            await WaitForAphriditeToStart()
-                .WaitAsync(TimeSpan.FromMinutes(20), _startCancellation.Token);
-            AddOutput("Aphrodite started!");
-        }
-        catch (TimeoutException e)
-        {
-            AddOutput("Failed to start Aphrodite: timeout.");
-        }
-        catch (TaskCanceledException e)
-        {
-            AddOutput("Aphrodite start cancelled.");
-        }
-        catch (Exception e)
-        {
-            AddOutput(e.ToString());
-        }
-    }
-
-    private List<string> GetWorkerOutput()
-    {
-        return _gridWorkerController.GridTextWorkerOutput;
-    }
-
-    private async Task StartAphrodite()
-    {
-        var textWorkerConfig = await _textWorkerConfigManager.LoadConfig();
-        string ModelName = textWorkerConfig.model_name;
-        string HuggingFaceToken = textWorkerConfig.hugging_face_token;
-
-        string command = $"(docker container stop aphrodite-engine || ver > nul)" +
-                         $"&& (docker rm aphrodite-engine || ver > nul)" +
-                         $"&& docker run -d -p 2242:7860 --network ai_network --gpus all --shm-size 8g" +
-                         $" -e MODEL_NAME={ModelName}" +
-                         $" -e KOBOLD_API=true" +
-                         $" -e GPU_MEMORY_UTILIZATION=0.9" +
-                         $" -e HF_TOKEN={HuggingFaceToken}" +
-                         $" --name aphrodite-engine alpindale/aphrodite-engine";
-
-        AddOutput("");
-        AddOutput("To continue, run the following command:");
-        AddOutput(command);
-        AddOutput("");
-    }
-
-    private async Task WaitForAphriditeToStart()
-    {
-        // Check if there is any response from get on port 7860
-        string address = "http://aphrodite-engine:7860";
-
-        while (!_startCancellation.IsCancellationRequested)
-        {
-            try
-            {
-                using HttpClient client = new HttpClient();
-                HttpResponseMessage response = await client.GetAsync(address);
-
-                if (response.IsSuccessStatusCode)
-                {
-                    AddOutput("Aphrodite started successfully.");
-                    break;
-                }
-            }
-            catch (HttpRequestException e)
-            {
-            }
-            catch (Exception e)
-            {
-                AddOutput(e.ToString());
-            }
-
-            await Task.Delay(1000);
-        }
-    }
-
+    
     private void AddOutput(string output)
     {
         Output.Add(output);
