0bf0bace523cc92901b9fb3c829beca845ad1baf
mateusz.krzaczek <mateusz.krzaczek@gmail.com>
2024-07-31 19:44:34 +0200

    Deleted dotnet omniworker

--- Aipg-omniworker-dotnet/AipgOmniworker/OmniController/StringListExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AipgOmniworker.OmniController;
-
-public static class StringListExtensions
-{
-    public static string ToOutputString(this List<string> list)
-    {
-        return string.Join("\n", list);
-    }
-}
